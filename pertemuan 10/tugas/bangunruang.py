63d5bbaeefa98d13df108c557999db340b6474e3
raflinaufal2704-arch <raflinaufal2704@gmail.com>
2025-11-26 11:43:54 +0700

    Add files via upload

---
+++ pertemuan 10/tugas/bangunruang.py
@@ -0,0 +1,16 @@
+import math
+
+def kubus(sisi):
+   return sisi**3
+
+def balok(p, l, t):
+   return p*l*t
+
+def prisma(luas, tinggi):
+   return luas*tinggi
+
+def tabung(jarijari, tinggi):
+   return math.pi*jarijari**2*tinggi
+
+def kerucut(jarijari, tinggi):
+   return (1/3)*math.pi*jarijari*jarijari*tinggi
